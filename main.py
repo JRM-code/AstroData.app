e6a91a3a9ece3bb8a801b3ff7bfeef40dc5cf881
JRM-code <83676722+JRM-code@users.noreply.github.com>
2026-03-31 14:14:35 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt 
+from astropy.timeseries import LombScargle
 
 # --- PAGE CONFIG AND TITLE --- #
 st.set_page_config(page_title = "V603 Aql 2021 Campaign Data", layout = "wide")
 st.title("V603 Aql 2021 campaign data")
-st.sidebar.title("Data Selectors")
 
 # --- CACHING FOR DATA SET LOADING --- #
 @st.cache_data
@@ -30,22 +31,26 @@ filt_data = camp_data[(camp_data['JD'] >= start_date) & (camp_data['JD'] <= end_
 # --- PLOT DATA BY DATE AND MAGNITUDE --- #
 x = filt_data['JD']
 y = filt_data['Magnitude']
+total_obs = filt_data['Magnitude'].size
 
 fig, ax = plt.subplots(figsize=(12, 4))
 ax.plot(x, y, ',', color='r')
+plt.xlabel("Julian Date")
+plt.ylabel("Magnitude")
 st.pyplot(fig)
+st.write(f"Total Observations for date range: {total_obs}")
 
+st.subheader("Plot 2: All data for selected filter")
 # --- SELECT DATA BY OBSERVING FILTER --- # 
-obs_filter = st.sidebar.selectbox('Plot 2 Filter Selector', options=camp_data['Band'].unique())
+obs_filter = st.selectbox('Plot 2 Filter Selector', options=camp_data['Band'].unique(), label_visibility="hidden")
 filter_data = camp_data[camp_data['Band'] == obs_filter]
-
 # --- PLOT FILTER DATA BY JD AND MAG --- #
 x1 = filter_data['JD']
 y1 = filter_data['Magnitude']
-
-st.subheader("Plot 2: All data for selected filter")
 fig, ax = plt.subplots(figsize=(12, 4))
 ax.plot(x1, y1, 'o', markersize=1, color='r')
+plt.xlabel("Julian Date")
+plt.ylabel("Magnitude")
 st.pyplot(fig)
 
 # --- INITIALISE SESSION STATE FOR DATA VISABILITY --- #
@@ -64,17 +69,17 @@ if st.session_state.show_data:
 else:
     st.write("Data table is hidden")
 
+st.subheader("Plot 3: All data for selected observer")
 # --- SELECT DATA BY OBSERVER --- # 
-observer = st.sidebar.selectbox('Plot 3 Observer Selector', options=camp_data['Observer Code'].unique())
+observer = st.selectbox('Plot 3 Observer Selector', options=camp_data['Observer Code'].unique(),label_visibility="hidden")
 obs_data = camp_data[camp_data['Observer Code'] == observer]
-
 # --- PLOT OBSERVER DATA BY JD AND MAG --- #
 x2 = obs_data['JD']
 y2 = obs_data['Magnitude']
-
-st.subheader("Plot 3: All data for selected observer")
 fig, ax = plt.subplots(figsize=(12, 4))
 ax.plot(x2, y2, 'o', markersize=1, color='r')
+plt.xlabel("Julian Date")
+plt.ylabel("Magnitude")
 st.pyplot(fig)
 
 # --- INITIALISE SESSION STATE FOR DATA VISABILITY --- #
@@ -92,3 +97,17 @@ if st.session_state.sh_data:
     st.dataframe(df2)
 else:
     st.write("Data table is hidden")
+
+# --- DATA ANALYSIS --- #
+st.subheader("Data Analysis")
+st.write("This section needs to be searchable by date, band, observer and period timeframes (Make the default search window between 0 and 3 days). Also, print the 5 most powerful periods as per thesis code")
+t = obs_data["JD"]
+y = obs_data["Magnitude"]
+
+frequency, power = LombScargle(t, y).autopower()
+
+fig, ax = plt.subplots()
+ax.plot(frequency, power, color='r')
+plt.xlabel("Frequency(Days)")
+plt.ylabel("Power")
+st.pyplot(fig)
